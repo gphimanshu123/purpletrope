c0c327a77459e6c210ba769c6bb1adfa3782f445
Himanshu Gogoi <himanshu.gogoi21@gmail.com>
2025-11-09 23:54:28 +0530

    wi

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import sanity from "@sanity/astro";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server", // Required for embedded Sanity Studio
+  adapter: vercel({}),
   integrations: [
     react(),
     sanity({
@@ -15,4 +18,12 @@ export default defineConfig({
       studioBasePath: "/studio", // Enables embedded Sanity Studio at /studio route
     }),
   ],
+  vite: {
+    optimizeDeps: {
+      exclude: ["sanity", "@sanity/vision", "sanity/structure"],
+    },
+    ssr: {
+      noExternal: ["sanity", "@sanity/vision"],
+    },
+  },
 });
